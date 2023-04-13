ad6eaec344477a932da12de1e68f842cbe2b5ac6
dpiedras <david-cpr@hotmail.com>
2023-04-12 22:19:32 -0500

    k dise

---
+++ prueba.js
@@ -0,0 +1,16 @@
+/*
+
+Diferencias entre dos strings
+
+Escribe una función llamada `distancia` que reciba dos strings del mismo tamaño y retorne el número de posiciones que tengan caracteres 
+diferentes.
+
+
+    distancia("hola", "pola") -> 1
+    distancia("sol", "tal") -> 2
+*/
+
+function distancia(w1,w2) {
+    
+
+}
